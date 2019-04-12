96d801803ce2047fe37c8950f3b95312cf82aaa9
George Kiagiadakis <george.kiagiadakis@collabora.com>
2019-04-12 12:53:54 +0300

    switch all headers to use SPDX-License-Identifier

--- lib/wp/error.h
+++ lib/wp/error.h
@@ -3,19 +3,8 @@
  * Copyright © 2019 Collabora Ltd.
  *    @author George Kiagiadakis <george.kiagiadakis@collabora.com>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
 
 #ifndef __WP_ERROR_H__
 #define __WP_ERROR_H__
