cf0e68a0f43bde96506dfe5c41278148877f0b9f
Julian Bouzas <julian.bouzas@collabora.com>
2019-06-28 12:34:03 -0400

    factory: remove unneeded sync construction API

--- lib/wp/factory.h
+++ lib/wp/factory.h
@@ -18,27 +18,19 @@ G_BEGIN_DECLS
 #define WP_TYPE_FACTORY (wp_factory_get_type ())
 G_DECLARE_FINAL_TYPE (WpFactory, wp_factory, WP, FACTORY, GObject)
 
-typedef gpointer (*WpFactoryFunc) (WpFactory * self, GType type,
-    GVariant * properties);
-typedef void (*WpFactoryAsyncFunc) (WpFactory * self, GType type,
+typedef void (*WpFactoryFunc) (WpFactory * self, GType type,
     GVariant * properties, GAsyncReadyCallback ready, gpointer user_data);
 
 WpFactory * wp_factory_new (WpCore * core, const gchar * name,
     WpFactoryFunc func);
-WpFactory * wp_factory_new_async (WpCore * core, const gchar * name,
-    WpFactoryAsyncFunc func);
 
 const gchar * wp_factory_get_name (WpFactory * self);
 WpCore * wp_factory_get_core (WpFactory * self);
-gpointer wp_factory_create_object (WpFactory * self, GType type,
-    GVariant * properties);
-void wp_factory_create_object_async (WpFactory * self, GType type,
+void wp_factory_create_object (WpFactory * self, GType type,
     GVariant * properties, GAsyncReadyCallback ready, gpointer user_data);
 
 WpFactory * wp_factory_find (WpCore * core, const gchar * name);
-gpointer wp_factory_make (WpCore * core, const gchar * name, GType type,
-    GVariant * properties);
-void wp_factory_make_async (WpCore * core, const gchar * name, GType type,
+void wp_factory_make (WpCore * core, const gchar * name, GType type,
     GVariant * properties, GAsyncReadyCallback ready, gpointer user_data);
 
 G_END_DECLS
