3396470f0141cfbeb0175a1de6bcf05b93b6e7e2
Julian Bouzas <julian.bouzas@collabora.com>
2020-01-08 10:24:39 -0500

    modules: simplify config endpoint module

--- modules/module-config-endpoint.c
+++ modules/module-config-endpoint.c
@@ -6,34 +6,13 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <pipewire/pipewire.h>
-
 #include <wp/wp.h>
 
 #include "module-config-endpoint/context.h"
 
-struct module_data
-{
-  WpConfigEndpointContext *ctx;
-};
-
-static void
-module_destroy (gpointer d)
-{
-  struct module_data *data = d;
-  g_clear_object (&data->ctx);
-  g_slice_free (struct module_data, data);
-}
-
 void
 wireplumber__module_init (WpModule * module, WpCore * core, GVariant * args)
 {
-  struct module_data *data;
-
-  /* Create the module data */
-  data = g_slice_new0 (struct module_data);
-  data->ctx = wp_config_endpoint_context_new (core);
-
-  /* Set the module destroy callback */
-  wp_module_set_destroy_callback (module, module_destroy, data);
+  WpConfigEndpointContext *ctx = wp_config_endpoint_context_new (core);
+  wp_module_set_destroy_callback (module, g_object_unref, ctx);
 }
