666cd10b6d11b5ba314f48a5453fa52e24803420
George Kiagiadakis <george.kiagiadakis@collabora.com>
2020-05-25 19:06:52 +0300

    algorithms: cleanup unused code

--- modules/module-si-adapter/algorithms.h
+++ modules/module-si-adapter/algorithms.h
@@ -6,10 +6,6 @@
  * SPDX-License-Identifier: MIT
  */
 
-typedef void (*CreateLinkCb) (WpProperties *, gpointer);
-gboolean multiport_link_create (GVariant * src_data, GVariant * sink_data,
-    CreateLinkCb create_link_cb, gpointer user_data, GError ** error);
-
 struct spa_audio_info_raw;
 gboolean choose_sensible_raw_audio_format (WpIterator *formats,
     gint channels_preference, struct spa_audio_info_raw *result);
