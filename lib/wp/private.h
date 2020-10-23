6b404516574f615917e3f834fd9a34813e87705f
George Kiagiadakis <george.kiagiadakis@collabora.com>
2020-10-23 16:59:19 +0300

    lib/private: move the registry & global APIs to a separate header

--- lib/wp/private.h
+++ lib/wp/private.h
@@ -18,6 +18,7 @@
 #include "si-interfaces.h"
 #include "iterator.h"
 #include "spa-type.h"
+#include "private/registry.h"
 
 #include <stdint.h>
 #include <pipewire/pipewire.h>
@@ -26,87 +27,6 @@ G_BEGIN_DECLS
 
 struct spa_pod;
 struct spa_pod_builder;
-
-typedef struct _WpRegistry WpRegistry;
-typedef struct _WpGlobal WpGlobal;
-
-/* registry */
-
-struct _WpRegistry
-{
-  struct pw_registry *pw_registry;
-  struct spa_hook listener;
-
-  GPtrArray *globals; // elementy-type: WpGlobal*
-  GPtrArray *tmp_globals; // elementy-type: WpGlobal*
-  GPtrArray *objects; // element-type: GObject*
-  GPtrArray *object_managers; // element-type: WpObjectManager*
-};
-
-void wp_registry_init (WpRegistry *self);
-void wp_registry_clear (WpRegistry *self);
-void wp_registry_attach (WpRegistry *self, struct pw_core *pw_core);
-void wp_registry_detach (WpRegistry *self);
-
-void wp_registry_prepare_new_global (WpRegistry * self, guint32 id,
-    guint32 permissions, guint32 flag, GType type,
-    WpProxy *proxy, const struct spa_dict *props,
-    WpGlobal ** new_global);
-
-gpointer wp_registry_find_object (WpRegistry *reg, GEqualFunc func,
-    gconstpointer data);
-void wp_registry_register_object (WpRegistry *reg, gpointer obj);
-void wp_registry_remove_object (WpRegistry *reg, gpointer obj);
-
-WpCore * wp_registry_get_core (WpRegistry * self) G_GNUC_CONST;
-
-/* core */
-
-WpRegistry * wp_core_get_registry (WpCore * self) G_GNUC_CONST;
-
-/* global */
-
-typedef enum {
-  WP_GLOBAL_FLAG_APPEARS_ON_REGISTRY = 0x1,
-  WP_GLOBAL_FLAG_OWNED_BY_PROXY = 0x2,
-} WpGlobalFlags;
-
-struct _WpGlobal
-{
-  guint32 flags;
-  guint32 id;
-  GType type;
-  guint32 permissions;
-  WpProperties *properties;
-  WpProxy *proxy;
-  WpRegistry *registry;
-};
-
-#define WP_TYPE_GLOBAL (wp_global_get_type ())
-GType wp_global_get_type (void);
-
-static inline void
-wp_global_clear (WpGlobal * self)
-{
-  g_clear_pointer (&self->properties, wp_properties_unref);
-}
-
-static inline WpGlobal *
-wp_global_ref (WpGlobal * self)
-{
-  return g_rc_box_acquire (self);
-}
-
-static inline void
-wp_global_unref (WpGlobal * self)
-{
-  g_rc_box_release_full (self, (GDestroyNotify) wp_global_clear);
-}
-
-void wp_global_rm_flag (WpGlobal *global, guint rm_flag);
-struct pw_proxy * wp_global_bind (WpGlobal * global);
-
-G_DEFINE_AUTOPTR_CLEANUP_FUNC (WpGlobal, wp_global_unref)
 
 /* props */
 
