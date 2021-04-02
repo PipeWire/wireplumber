9f77b98b103dcc44036de25f6df849171c90201a
Julian Bouzas <julian.bouzas@collabora.com>
2021-04-02 13:01:24 -0400

    src: scripts: add static-endpoints.lua script

--- src/config/config.lua.d/10-default-policy.lua
+++ src/config/config.lua.d/10-default-policy.lua
@@ -8,6 +8,28 @@ default_policy.sessions = {
   ["video"] = { ["media.type"] = "Video" },
 }
 
+default_policy.endpoints = {
+  -- [endpoint name] = { endpoint properties }
+  ["endpoint.music"] = {
+    ["media.class"] = "Audio/Sink",
+    ["role"] = "Music",
+    ["priority"] = 0,
+    ["session.name"] = "audio",
+  },
+  ["endpoint.notifications"] = {
+    ["media.class"] = "Audio/Sink",
+    ["role"] = "Notifications",
+    ["priority"] = 50,
+    ["session.name"] = "audio",
+  },
+  ["endpoint.voice"] = {
+    ["media.class"] = "Audio/Source",
+    ["role"] = "Voice",
+    ["priority"] = 90,
+    ["session.name"] = "audio",
+  },
+}
+
 default_policy.policy = {
   move = true,   -- moves session items when metadata target.node changes
   follow = true  -- moves session items to the default device when it has changed
@@ -19,9 +41,13 @@ function default_policy.enable()
   load_module("si-node")
   load_module("si-audio-adapter")
   load_module("si-standard-link")
+  load_module("si-audio-endpoint")
 
   -- Create sessions statically at startup
   load_script("static-sessions.lua", default_policy.sessions)
+
+  -- Create endpoints statically at startup
+  load_script("static-endpoints.lua", default_policy.endpoints)
 
   -- Create items for nodes that appear in the graph
   load_script("create-item.lua")
