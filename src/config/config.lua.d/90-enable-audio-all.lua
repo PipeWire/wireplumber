28a422968191278640436ca1b092baeada75e262
George Kiagiadakis <george.kiagiadakis@collabora.com>
2021-04-27 19:50:38 +0300

    config: unify config and config-split
    After all, we can just load multiple lua config files/directories
    from a single .conf file and so have both styles available for the
    user to choose at runtime

--- src/config/config.lua.d/90-enable-audio-all.lua
+++
@@ -1,6 +0,0 @@
--- Enable local & bluetooth audio devices
-alsa_monitor.enable()
-bluez_monitor.enable()
-
--- Enables functionality to save and restore default device profiles
-load_module("default-profile")
