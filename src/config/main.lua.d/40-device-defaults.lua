ee87161d291ceec8a01b088698fa9b7ab71de8f1
George Kiagiadakis <george.kiagiadakis@collabora.com>
2021-06-03 16:26:28 +0300

    default-routes: re-implement the default-routes module in lua
    using the logic from the default-routes of pipewire-media-session
    
    Fixes: #28, #30
    Related: !156

--- src/config/main.lua.d/40-device-defaults.lua
+++ src/config/main.lua.d/40-device-defaults.lua
@@ -1,18 +1,22 @@
 device_defaults = {}
 
 device_defaults.properties = {
-  -- store preferences to the file system and restore them at startup
+  -- store preferences to the file system and restore them at startup;
+  -- when set to false, default nodes and routes are selected based on
+  -- their priorities and any runtime changes do not persist after restart
   ["use-persistent-storage"] = true,
 }
 
 function device_defaults.enable()
-  -- Enables saving and restoring default nodes
+  -- Selects appropriate default nodes and enables saving and restoring them
   load_module("default-nodes", device_defaults.properties)
 
-  if device_defaults.properties["use-persistent-storage"] then
-    -- Automatically save and restore default routes
-    load_module("default-routes")
+  -- Selects appropriate default routes ("ports" in pulseaudio terminology)
+  -- and enables saving and restoring them together with
+  -- their properties (per-route/port volume levels, channel maps, etc)
+  load_script("default-routes.lua", device_defaults.properties)
 
+  if device_defaults.properties["use-persistent-storage"] then
     -- Enables functionality to save and restore default device profiles
     load_module("default-profile")
   end
