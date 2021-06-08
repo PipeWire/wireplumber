0f01c8b6da23875827433610972bc83214407320
Julian Bouzas <julian.bouzas@collabora.com>
2021-06-08 11:57:49 -0400

    tests: si-standard-link: remove unneeded core syncs

--- tests/modules/si-standard-link.c
+++ tests/modules/si-standard-link.c
@@ -98,20 +98,8 @@ test_si_standard_link_setup (TestFixture * f, gconstpointer user_data)
 }
 
 static void
-on_core_sync_done (WpCore *core, GAsyncResult *res, TestFixture * f)
-{
-  g_autoptr (GError) error = NULL;
-  gboolean ret = wp_core_sync_finish (core, res, &error);
-  g_assert_no_error (error);
-  g_assert_true (ret);
-  g_main_loop_quit (f->base.loop);
-}
-
-static void
 test_si_standard_link_teardown (TestFixture * f, gconstpointer user_data)
 {
-  wp_core_sync (f->base.core, NULL, (GAsyncReadyCallback) on_core_sync_done, f);
-  g_main_loop_run (f->base.loop);
   g_clear_object (&f->sink_item);
   g_clear_object (&f->src_item);
   wp_base_test_fixture_teardown (&f->base);
@@ -137,8 +125,6 @@ test_si_standard_link_main (TestFixture * f, gconstpointer user_data)
   /* activate */
   wp_object_activate (WP_OBJECT (link), WP_SESSION_ITEM_FEATURE_ACTIVE,
       NULL, (GAsyncReadyCallback) test_object_activate_finish_cb, f);
-  g_main_loop_run (f->base.loop);
-  wp_core_sync (f->base.core, NULL, (GAsyncReadyCallback) on_core_sync_done, f);
   g_main_loop_run (f->base.loop);
 
   /* verify the graph state */
@@ -191,8 +177,6 @@ test_si_standard_link_main (TestFixture * f, gconstpointer user_data)
 
   /* deactivate */
   wp_object_deactivate (WP_OBJECT (link), WP_SESSION_ITEM_FEATURE_ACTIVE);
-  wp_core_sync (f->base.core, NULL, (GAsyncReadyCallback) on_core_sync_done, f);
-  g_main_loop_run (f->base.loop);
 
   /* verify the graph state */
   {
