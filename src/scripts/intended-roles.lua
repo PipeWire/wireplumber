5fba8af1e622952e5131bac3e82251c2c9e2a2f3
Sanchayan Maity <sanchayan@asymptotic.io>
2021-08-12 20:56:23 +0530

    scripts: intended-roles: Use both device and stream
    When a stream gets added, we might have a situation where the device
    does not yet exist to actually link on the basis of intended role. For
    example this can be observed if no mic sources are present and then the
    mic appears after a bluetooth profile switch to HFP.
    
    To fix this, iterate over streams when a device connects in addition to
    iterating over devices when a stream connects.

--- src/scripts/intended-roles.lua
+++ src/scripts/intended-roles.lua
@@ -31,33 +31,41 @@ streams_om = ObjectManager {
   }
 }
 
-streams_om:connect("object-added", function (streams_om, stream)
+local function routeUsingIntendedRole(stream, dev)
   local stream_role = stream.properties["media.role"]
   local is_input = stream.properties["media.class"]:find("Input") ~= nil
 
+  local is_source = dev.properties["media.class"]:find("Source") ~= nil
+  local dev_roles = dev.properties["device.intended-roles"]
+
+  -- Make sure the stream and device direction match
+  if is_input ~= is_source then
+    return
+  end
+
+  for role in dev_roles:gmatch("(%a+)") do
+    if role == stream_role then
+      Log.info(stream,
+        string.format("Routing stream '%s' (%d) with role '%s' to '%s' (%d)",
+          stream.properties["node.name"], stream["bound-id"], stream_role,
+          dev.properties["node.name"], dev["bound-id"])
+      )
+
+      local metadata = metadata_om:lookup()
+      metadata:set(stream["bound-id"], "target.node", "Spa:Id", dev["bound-id"])
+    end
+  end
+end
+
+streams_om:connect("object-added", function (streams_om, stream)
   for dev in devices_om:iterate() do
-    local is_source = dev.properties["media.class"]:find("Source") ~= nil
-    local dev_roles = dev.properties["device.intended-roles"]
+    routeUsingIntendedRole(stream, dev)
+  end
+end)
 
-    -- Make sure the stream and device direction match
-    if is_input ~= is_source then
-      goto next
-    end
-
-    for role in dev_roles:gmatch("(%a+)") do
-      if role == stream_role then
-        Log.info(stream,
-          string.format("Routing stream '%s' (%d) with role '%s' to '%s' (%d)",
-            stream.properties["node.name"], stream["bound-id"], stream_role,
-            dev.properties["node.name"], dev["bound-id"])
-        )
-
-        local metadata = metadata_om:lookup()
-        metadata:set(stream["bound-id"], "target.node", "Spa:Id", dev["bound-id"])
-      end
-    end
-
-    ::next::
+devices_om:connect("object-added", function (devices_om, dev)
+  for stream in streams_om:iterate() do
+    routeUsingIntendedRole(stream, dev)
   end
 end)
 
