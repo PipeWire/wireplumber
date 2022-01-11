99dfa7d38a66943fc04181a2b82fe6da91402c4f
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2022-01-11 15:30:30 +0530

    config: update the endpoints config
    -Sync endpoints config from AGL.
    -Add a capture endpoint.

--- src/config/policy.lua.d/50-endpoints-config.lua
+++ src/config/policy.lua.d/50-endpoints-config.lua
@@ -4,36 +4,92 @@
 --[[
 
 default_policy.policy.roles = {
+  ["Capture"] = {
+    ["alias"] = { "Multimedia", "Music", "Voice", "Capture" },
+    ["priority"] = 25,
+    ["action.default"] = "cork",
+    ["action.capture"] = "mix",
+    ["media.class"] = "Audio/Source",
+  },
   ["Multimedia"] = {
     ["alias"] = { "Movie", "Music", "Game" },
-    ["priority"] = 10,
-    ["action.default"] = "mix",
+    ["priority"] = 25,
+    ["action.default"] = "cork",
   },
-  ["Notification"] = {
-    ["priority"] = 20,
-    ["action.default"] = "duck",
-    ["action.Notification"] = "mix",
-  },
-  ["Alert"] = {
+  ["Speech-Low"] = {
     ["priority"] = 30,
     ["action.default"] = "cork",
-    ["action.Alert"] = "mix",
+    ["action.Speech-Low"] = "mix",
+  },
+  ["Custom-Low"] = {
+    ["priority"] = 35,
+    ["action.default"] = "cork",
+    ["action.Custom-Low"] = "mix",
+  },
+  ["Navigation"] = {
+    ["priority"] = 50,
+    ["action.default"] = "duck",
+    ["action.Navigation"] = "mix",
+  },
+  ["Speech-High"] = {
+    ["priority"] = 60,
+    ["action.default"] = "cork",
+    ["action.Speech-High"] = "mix",
+  },
+  ["Custom-High"] = {
+    ["priority"] = 65,
+    ["action.default"] = "cork",
+    ["action.Custom-High"] = "mix",
+  },
+  ["Communication"] = {
+    ["priority"] = 75,
+    ["action.default"] = "cork",
+    ["action.Communication"] = "mix",
+  },
+  ["Emergency"] = {
+    ["alias"] = { "Alert" },
+    ["priority"] = 99,
+    ["action.default"] = "cork",
+    ["action.Emergency"] = "mix",
   },
 }
 
 default_policy.endpoints = {
+  ["endpoint.capture"] = {
+    ["media.class"] = "Audio/Source",
+    ["role"] = "Capture",
+  },
   ["endpoint.multimedia"] = {
     ["media.class"] = "Audio/Sink",
     ["role"] = "Multimedia",
   },
-  ["endpoint.notifications"] = {
+  ["endpoint.speech_low"] = {
     ["media.class"] = "Audio/Sink",
-    ["role"] = "Notification",
+    ["role"] = "Speech-Low",
   },
-  ["endpoint.alert"] = {
+  ["endpoint.custom_low"] = {
     ["media.class"] = "Audio/Sink",
-    ["role"] = "Alert",
+    ["role"] = "Custom-Low",
+  },
+  ["endpoint.navigation"] = {
+    ["media.class"] = "Audio/Sink",
+    ["role"] = "Navigation",
+  },
+  ["endpoint.speech_high"] = {
+    ["media.class"] = "Audio/Sink",
+    ["role"] = "Speech-High",
+  },
+  ["endpoint.custom_high"] = {
+    ["media.class"] = "Audio/Sink",
+    ["role"] = "Custom-High",
+  },
+  ["endpoint.communication"] = {
+    ["media.class"] = "Audio/Sink",
+    ["role"] = "Communication",
+  },
+  ["endpoint.emergency"] = {
+    ["media.class"] = "Audio/Sink",
+    ["role"] = "Emergency",
   },
 }
-
 ]]--
