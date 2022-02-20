b8b3f171813a93592ecf33fcee5035286af829e8
Marko Gobin <marko.gobin@ag04.com>
2022-02-20 16:03:16 +0100

    config: add more chromium based browsers to media-role.applications

--- src/config/policy.lua.d/10-default-policy.lua
+++ src/config/policy.lua.d/10-default-policy.lua
@@ -28,7 +28,7 @@ bluetooth_policy.policy = {
   -- Application names correspond to application.name in stream properties.
   -- Applications which do not set media.role but which should be considered
   -- for role based profile switching can be specified here.
-  ["media-role.applications"] = { "Chromium input", "Firefox", "Google Chrome input", "ZOOM VoiceEngine" },
+  ["media-role.applications"] = { "Firefox", "Chromium input", "Google Chrome input", "Brave input", "Microsoft Edge input", "Vivaldi input", "ZOOM VoiceEngine" },
 }
 
 function default_policy.enable()
