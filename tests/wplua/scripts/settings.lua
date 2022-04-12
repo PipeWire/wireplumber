277824dcb2d7ebc2d18297ca90ca87a2655e12c7
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2022-04-12 11:20:57 +0530

    lib: Add parsing logic for rules
    - unlike pipewire, rules will be parsed during the bootup time, i.e
      during the creation of the wpsettings object.
    - The rules are parsed into the wpinterest objects and stored in the
      wpsettings object, they will be eventually used to service the
      apply_rule() API.
    - Support pipewire syntax of rules defination in this version.

--- tests/wplua/scripts/settings.lua
+++ tests/wplua/scripts/settings.lua
@@ -2,7 +2,7 @@
 --  tests the lua API of WpSettings, this file tests the settings present in
 --  settings.conf
 
-assert (Settings.get_boolean("property1", "test-settings") == false)
-assert (Settings.get_boolean("property2", "test-settings") == true)
+assert (Settings.get_boolean("test-property1", "test-settings") == false)
+assert (Settings.get_boolean("test-property2", "test-settings") == true)
 
-assert (Settings.get_boolean("property1") == false)
+assert (Settings.get_boolean("test-property1") == false)
