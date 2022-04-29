8dc4c36dd4b004ddaa0bad2c02817d44ba4db3dd
Sven Bartscher <sven.bartscher@credativ.de>
2022-04-29 13:20:12 +0200

    Enable automatic bluetooth profile switching for Mumble
    For some reason Mumble uses "game" as its media.role, but it is
    actually a communication tool where a headset profile is apropriate.

--- src/config/policy.lua.d/10-default-policy.lua
+++ src/config/policy.lua.d/10-default-policy.lua
@@ -34,7 +34,7 @@ bluetooth_policy.policy = {
   -- Application names correspond to application.name in stream properties.
   -- Applications which do not set media.role but which should be considered
   -- for role based profile switching can be specified here.
-  ["media-role.applications"] = { "Firefox", "Chromium input", "Google Chrome input", "Brave input", "Microsoft Edge input", "Vivaldi input", "ZOOM VoiceEngine", "Telegram Desktop", "telegram-desktop", "linphone" },
+  ["media-role.applications"] = { "Firefox", "Chromium input", "Google Chrome input", "Brave input", "Microsoft Edge input", "Vivaldi input", "ZOOM VoiceEngine", "Telegram Desktop", "telegram-desktop", "linphone", "Mumble" },
 }
 
 function default_policy.enable()
