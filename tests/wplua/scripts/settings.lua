95beabe6f95054920e1612c0d9124069318e1fa3
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2022-05-09 12:43:36 +0530

    lib: Add _get_float() wpsettings API
    - also add the corresponding C & Lua tests.

--- tests/wplua/scripts/settings.lua
+++ tests/wplua/scripts/settings.lua
@@ -7,6 +7,7 @@ local value = Settings.get_boolean ("test-property1", "test-settings")
 assert (value == false)
 
 value = Settings.get_boolean ("test-property2", "test-settings")
+assert ("boolean" == type (value))
 assert (value == true)
 
 value = Settings.get_boolean ("test-property1")
@@ -22,6 +23,7 @@ value = Settings.get_int ("test-property-undefined", "test-settings")
 assert (value == nil)
 
 value = Settings.get_int ("test-property3-int", "test-settings")
+assert ("number" == type (value))
 assert (value == -20)
 
 value = Settings.get_int ("test-property4-max-int", "test-settings")
@@ -43,10 +45,23 @@ value = Settings.get_string ("test-property-undefined", "test-settings")
 assert (value == nil)
 
 value = Settings.get_string ("test-property4-string", "test-settings")
+assert ("string" == type (value))
 assert (value == "blahblah")
 
 value = Settings.get_string ("test-property3-int", "test-settings")
 assert (value == "-20")
+
+-- test settings _get_float ()
+value = Settings.get_float ("test-property-undefined", "test-settings")
+assert (value == nil)
+
+value = Settings.get_float ("test-property-float1", "test-settings")
+assert ("number" == type (value))
+assert ((value - 3.14) < 0.00001)
+
+value = Settings.get_float ("test-property-float2", "test-settings")
+assert ((value - 0.4) < 0.00001)
+
 
 -- test rules
 -- test #1
@@ -147,3 +162,14 @@ assert (ap["prop.electrical.conductivity"] == "false")
 assert (ap["prop.state"] == "gas")
 assert (ap["prop.example"] == "neon")
 
+-- test #9
+local cp = {
+  ["test-string6-wildcard"] = "wild_cat",
+}
+
+local applied, ap = Settings.apply_rule ("rule_three", cp, "test-settings")
+
+assert (applied == true)
+assert (ap["prop.electrical.conductivity"] == "true")
+assert (ap["prop.state"] == "solid")
+assert (ap["prop.example"] == "ferrous")
