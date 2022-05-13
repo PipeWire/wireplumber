b205914515c1109a6be846f9914b3210c4492ff5
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2022-05-13 06:08:50 +0530

    bluez.lua: Switch bluez lua monitor to wpsettings
    - Monitor to use the JSON APIs and move away from config/lua
    - Enhance logic in wpsettings to parse settings which are JSON arrays,
      some of the bluetooth properties are JSON arrays and parsing
      logic confused them for a rule.
    - Add corresponding tests around this logic as well.

--- src/config/bluetooth.lua.d/30-bluez-monitor.lua
+++ src/config/bluetooth.lua.d/30-bluez-monitor.lua
@@ -3,10 +3,10 @@ bluez_monitor.properties = {}
 bluez_monitor.rules = {}
 
 function bluez_monitor.enable()
-  load_monitor("bluez", {
-    properties = bluez_monitor.properties,
-    rules = bluez_monitor.rules,
-  })
+  -- load_monitor("bluez", {
+  --   properties = bluez_monitor.properties,
+  --   rules = bluez_monitor.rules,
+  -- })
 
   if bluez_monitor.properties["with-logind"] then
     load_optional_module("logind")
