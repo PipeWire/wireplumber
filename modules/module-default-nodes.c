6625c1e42f40d0088f9e029dcf869b62a0da0768
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2022-07-04 06:15:00 +0530

    m-default-nodes*: Remove suffixes in hooks name
    Suffix is redundant for on events as Events names and Event chains are
    printed with 847c0.
    
    Update the settings file used.

--- modules/module-default-nodes.c
+++ modules/module-default-nodes.c
@@ -38,6 +38,10 @@ enum {
  * default.configured.*(default-configured-nodes) keys.
  */
 
+/*
+ * settings file: device-settings.conf
+ */
+
 typedef struct _WpDefaultNode WpDefaultNode;
 struct _WpDefaultNode
 {
@@ -466,7 +470,7 @@ wp_default_nodes_enable (WpPlugin * plugin, WpTransition * transition)
   g_return_if_fail (dispatcher);
 
   /* default metadata added */
-  hook = wp_simple_event_hook_new ("default-nodes-on-metadata-added", 10,
+  hook = wp_simple_event_hook_new ("default-nodes", 10,
       WP_EVENT_HOOK_EXEC_TYPE_ON_EVENT,
       g_cclosure_new ((GCallback) on_metadata_added, self, NULL));
   wp_interest_event_hook_add_interest (WP_INTEREST_EVENT_HOOK (hook),
@@ -478,7 +482,7 @@ wp_default_nodes_enable (WpPlugin * plugin, WpTransition * transition)
   g_clear_object(&hook);
 
   /* default metadata changed */
-  hook = wp_simple_event_hook_new ("default-nodes-on-metadata-changed", 10,
+  hook = wp_simple_event_hook_new ("default-nodes", 10,
       WP_EVENT_HOOK_EXEC_TYPE_ON_EVENT,
       g_cclosure_new ((GCallback) on_metadata_changed, self, NULL));
   wp_interest_event_hook_add_interest (WP_INTEREST_EVENT_HOOK (hook),
--- src/scripts/policy-node.lua
+++ src/scripts/policy-node.lua
@@ -957,7 +957,7 @@ end
 
 SimpleEventHook {
   priority = 15,
-  name = "policy-handle-linkable",
+  name = "policy-node",
   type = "on-event",
   interests = {
     EventInterest {
@@ -983,7 +983,7 @@ SimpleEventHook {
 
 SimpleEventHook {
   priority = 5,
-  name = "policy-unhandle-linkable",
+  name = "policy-node",
   type = "on-event",
   interests = {
     EventInterest {
@@ -1002,7 +1002,7 @@ SimpleEventHook {
 
 SimpleEventHook {
   priority = 1,
-  name = "policy-rescanner",
+  name = "policy-node-rescanner",
   type = "after-events",
   interests = {
     -- on audio device node linkable addition
