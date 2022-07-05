300013f8c434b7cbf56983403fb5fdfc35b022fc
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2022-07-06 05:17:43 +0530

    event-dispatcher: Add an enumeration of default event hook priorities
    This scheme provides for an orderly execution of hooks as the priorities
    are controlled from one single place. Enumeration is defined in such a
    way that new items can be added easily.
    
    All the event hooks are changed to get the priorities from this
    enumeration.

--- src/scripts/policy-node.lua
+++ src/scripts/policy-node.lua
@@ -958,9 +958,9 @@ function checkFiltersPortsState (si)
 end
 
 SimpleEventHook {
-  priority = 15,
   name = "policy-node",
   type = "on-event",
+  priority = "linkable-added-create-item",
   interests = {
     EventInterest {
       Constraint { "event.type", "=", "object-added" },
@@ -984,9 +984,9 @@ SimpleEventHook {
 }:register()
 
 SimpleEventHook {
-  priority = 5,
   name = "policy-node",
   type = "on-event",
+  priority = "linkable-removed-create-item",
   interests = {
     EventInterest {
       Constraint { "event.type", "=", "object-removed" },
@@ -1003,8 +1003,8 @@ SimpleEventHook {
 }:register()
 
 SimpleEventHook {
-  priority = 1,
-  name = "policy-node-rescanner",
+  name = "rescan-policy",
+  priority = "rescan-policy",
   type = "after-events",
   interests = {
     -- on audio device node linkable addition
