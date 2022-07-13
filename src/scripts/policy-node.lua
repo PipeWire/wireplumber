96c82d699845012dc52c2737db916c6c61486ecd
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2022-07-13 06:27:08 +0530

    event-hooks: remove the unwanted second arg in simple hooks

--- src/scripts/policy-node.lua
+++ src/scripts/policy-node.lua
@@ -971,7 +971,7 @@ SimpleEventHook {
       Constraint { "active-features", "!", 0, type = "gobject" },
     },
   },
-  execute = function (event, transition)
+  execute = function (event)
     local si = event:get_subject()
     local si_props = si.properties
 
@@ -997,7 +997,7 @@ SimpleEventHook {
       Constraint { "active-features", "!", 0, type = "gobject" },
     },
   },
-  execute = function (event, transition)
+  execute = function (event)
     local si = event:get_subject()
     unhandleLinkable (si)
   end
