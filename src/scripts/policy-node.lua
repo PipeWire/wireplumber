d8758f0ad06a4f09e29b3bcf292c592f47e84886
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2022-07-15 09:57:01 +0530

    event-hooks: give unique names to event hooks

--- src/scripts/policy-node.lua
+++ src/scripts/policy-node.lua
@@ -985,7 +985,7 @@ function checkFiltersPortsState (si)
 end
 
 SimpleEventHook {
-  name = "policy-node",
+  name = "handle-linkable@policy-node",
   type = "on-event",
   priority = "linkable-added-create-item",
   interests = {
@@ -1011,7 +1011,7 @@ SimpleEventHook {
 }:register()
 
 SimpleEventHook {
-  name = "policy-node",
+  name = "linkable-removed@policy-node",
   type = "on-event",
   priority = "linkable-removed-create-item",
   interests = {
--- src/scripts/restore-stream.lua
+++ src/scripts/restore-stream.lua
@@ -398,7 +398,7 @@ end
 -- save "targe.node" if it is present in default metadata
 if config_restore_target then
   SimpleEventHook {
-    name = "restore-stream-save-target",
+    name = "metadata-added@restore-stream-save-target",
     type = "on-event",
     priority = "default-metadata-added-restore-stream",
     interests = {
@@ -420,7 +420,7 @@ if config_restore_target then
 
 -- save "target.node" on metadata changes
   SimpleEventHook {
-    name = "restore-stream-save-target",
+    name = "metadata-changed@restore-stream-save-target",
     type = "on-event",
     priority = "default-metadata-changed-restore-stream",
     interests = {
@@ -521,7 +521,7 @@ allnodes_om:activate ()
 
 -- restore-stream properties
 SimpleEventHook {
-  name = "restore-stream",
+  name = "node-added@restore-stream",
   type = "on-event",
   priority = "node-added-restore-stream",
   interests = {
@@ -551,7 +551,7 @@ SimpleEventHook {
 
 -- save-stream if any of the stream parms changes
 SimpleEventHook {
-  name = "restore-stream-save-stream",
+  name = "node-parms-changed@restore-stream-save-stream",
   type = "on-event",
   priority = "node-changed-restore-stream",
   interests = {
