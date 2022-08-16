abfd3aa4856de1958a0a1915906d6c2012e6fe8c
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2022-08-16 06:05:55 +0530

    policy-node.lua: Second round of cleanup.
    - WirePlumber Lua now facilitates Lua libraries/modules, utilize this and create
      modules. Add some tests around this functionality.
    - Create policy-hooks.lua containing all the hooks to find-target events
    - Create policy-utils.lua module and push all the policy utility functions to it.
    - Create common-utils.lua module and push the common utility functions to it.
    - Remove all the above functionality from policy-node.lua and clean it up.

--- tests/wplua/scripts/lib/testlib.lua
+++ tests/wplua/scripts/lib/testlib.lua
@@ -1,9 +1,26 @@
 local testlib = {}
 
+testlib.table1 = {}
+
+testlib.table1 ["test-key"] = "test-value"
+
 function testlib.test_add_ten (x)
   return x + 10
 end
 
-Log.info("in testlib")
+function testlib.get_empty_table (key)
+  testlib.table1 [key] = {}
+  return testlib.table1 [key]
+end
+
+function testlib.set_table (key, value)
+  testlib.table1 [key] = value
+end
+
+function testlib.get_table (key)
+  return testlib.table1 [key]
+end
+
+Log.info ("in testlib")
 
 return testlib
