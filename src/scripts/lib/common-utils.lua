62052b34f886611b2a623d134194be020cdbf989
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2022-08-16 06:05:55 +0530

    policy-node.lua: Second round of cleanup.
    - WirePlumber Lua now facilitates Lua libraries/modules, utilize this and create
      modules. Add some tests around this functionality.
    - Create policy-hooks.lua containing all the hooks to find-target events
    - Create policy-utils.lua module and push all the policy utility functions to it.
    - Create common-utils.lua module and push the common utility functions to it.
    - Remove all the above functionality from policy-node.lua and clean it up.

---
+++ src/scripts/lib/common-utils.lua
@@ -0,0 +1,37 @@
+-- WirePlumber
+
+-- Copyright © 2022 Collabora Ltd.
+--    @author Ashok Sidipotu <ashok.sidipotu@collabora.com>
+
+-- SPDX-License-Identifier: MIT
+
+-- Script is a Lua Module of common Lua utility functions
+
+local cutils = {}
+
+function cutils.parseBool (var)
+  return var and (var:lower () == "true" or var == "1")
+end
+
+function cutils.getTargetDirection (properties)
+  local target_direction = nil
+  if properties ["item.node.direction"] == "output" or
+      (properties ["item.node.direction"] == "input" and
+          cutils.parseBool (properties ["stream.capture.sink"])) then
+    target_direction = "input"
+  else
+    target_direction = "output"
+  end
+  return target_direction
+end
+
+function cutils.getDefaultNode (properties, target_direction)
+  local target_media_class =
+  properties ["media.type"] ..
+      (target_direction == "input" and "/Sink" or "/Source")
+  return default_nodes:call ("get-default-node", target_media_class)
+end
+
+default_nodes = Plugin.find ("default-nodes-api")
+
+return cutils
--- tests/wplua/scripts/require.lua
+++ tests/wplua/scripts/require.lua
@@ -1,8 +1,19 @@
-local testlib = require("testlib")
+local testlib = require ("testlib")
+assert (package.loaded ["testlib"] == testlib)
 
-assert(type(testlib) == "table")
-assert(package.loaded["testlib"] == testlib)
+assert (type (testlib) == "table")
 
 local x = 1
-x = testlib.test_add_ten(x)
-assert(x == 11)
+x = testlib.test_add_ten (x)
+assert (x == 11)
+
+local val_table = testlib.get_empty_table ("test-key")
+assert (type (val_table) == "table")
+val_table ["key"] = "value"
+val_table.id = 100
+
+testlib.set_table ("test-key", val_table)
+
+local val1 = testlib.get_table ("test-key")
+assert (val1 ["key"] == "value")
+assert (val1.id == 100)
