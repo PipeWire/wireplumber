3a3e107cd2a99421c132edff0339ca731a715a83
Julian Bouzas <julian.bouzas@collabora.com>
2022-10-05 16:22:28 -0400

    access-default.lua: use default rules if none are defined

--- src/scripts/access/access-default.lua
+++ src/scripts/access/access-default.lua
@@ -5,12 +5,27 @@
 --
 -- SPDX-License-Identifier: MIT
 
-function rulesGetDefaultPermissions(properties)
+function getDefaultPermissions (properties)
+  local pw_access = properties["pipewire.access"]
+  local media_category = properties["media.category"]
+
+  if pw_access == "flatpak" and media_category == "Manager" then
+    return "all"
+  elseif pw_access == "flatpak" or pw_access == "restricted" then
+    return "rx"
+  end
+
+  return nil
+end
+
+function getPermissions (properties)
   local matched, mprops = Settings.apply_rule ("access.rules", properties)
 
   if (matched and mprops["default_permissions"]) then
     return mprops["default_permissions"]
   end
+
+  return nil
 end
 
 clients_om = ObjectManager {
@@ -21,9 +36,12 @@ clients_om:connect("object-added", function (om, client)
   local id = client["bound-id"]
   local properties = client["properties"]
 
-  local perms = rulesGetDefaultPermissions(properties)
+  local perms = getPermissions (properties)
+  if perms == nil then
+    perms = getDefaultPermissions (properties)
+  end
 
-  if perms then
+  if perms ~= nil then
     Log.info(client, "Granting permissions to client " .. id .. ": " .. perms)
     client:update_permissions { ["any"] = perms }
   end
