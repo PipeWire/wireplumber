1d80d6f03742f0edcb1bbf2fc91bbd9f92a30f94
George Kiagiadakis <george.kiagiadakis@collabora.com>
2022-11-08 12:56:42 +0200

    scripts: policy-node: use policy-config library

--- src/scripts/lib/policy-config.lua
+++ src/scripts/lib/policy-config.lua
@@ -9,16 +9,20 @@
 local defaults <const> = {
   move = true,
   follow = true,
+  filter_forward_format = false,
 }
 
 local keys <const> = {
   move = "policy.default.move",
   follow = "policy.default.follow",
+  filter_forward_format = "policy.default.filter-forward-format",
 }
 
 local config = {
   move = Settings.parse_boolean_safe (keys.move, defaults.move),
   follow = Settings.parse_boolean_safe (keys.follow, defaults.follow),
+  filter_forward_format = Settings.parse_boolean_safe (
+      keys.filter_forward_format, defaults.filter_forward_format),
 }
 
 Settings.subscribe ("policy.default*", function (_, setting, value)
@@ -30,6 +34,10 @@ Settings.subscribe ("policy.default*", function (_, setting, value)
   elseif setting == keys.follow then
     if type (parsed_val) == "boolean" and config.follow ~= parsed_val then
       config.follow = parsed_val
+    end
+  elseif setting == keys.filter_forward_format then
+    if type (parsed_val) == "boolean" and config.filter_forward_format ~= parsed_val then
+      config.filter_forward_format = parsed_val
     end
   end
 end)
