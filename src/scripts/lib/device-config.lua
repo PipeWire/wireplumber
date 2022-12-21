f9d885bff140890d2f75a99e384d91b82ab494d5
George Kiagiadakis <george.kiagiadakis@collabora.com>
2022-12-21 11:27:57 +0200

    default-nodes: port to a set of scripts with hooks

--- src/scripts/lib/device-config.lua
+++ src/scripts/lib/device-config.lua
@@ -12,6 +12,9 @@ local defaults = {
   ["use-persistent-storage"] = true,
   ["default-volume"] = 0.4 ^ 3,
   ["default-input-volume"] = 1.0,
+  ["auto-echo-cancel"] = true,
+  ["echo-cancel-sink-name"] = "echo-cancel-sink",
+  ["echo-cancel-source-name"] = "echo-cancel-source",
 }
 
 return settings_manager.new ("device.", defaults)
