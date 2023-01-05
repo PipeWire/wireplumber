9166463124b846890a41ab6146b904096fe41eed
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2023-01-05 20:41:44 +0530

    linking tests: add defined tests

---
+++ tests/scripts/scripts/07-test-linking-defined-device-node-target-object-string-capture.lua
@@ -0,0 +1,64 @@
+-- Tests linking of streams and defined devices. Three device nodes are created,
+-- among which only one is selected as the defined device(target.object).
+
+-- The target.object here is a node name
+local pu = require ("policy-utils")
+local tu = require ("test-utils")
+
+Script.async_activation = true
+
+tu.createDeviceNode ("nondefault-device-node", "Audio/Source")
+tu.createDeviceNode ("default-device-node", "Audio/Source")
+tu.createDeviceNode ("defined-device-node", "Audio/Source")
+
+tu.createStreamNode ("stream-node")
+
+-- hook to selet defined target
+SimpleEventHook {
+  name = "linkable-added@test-linking",
+  after = "linkable-added@test-utils-linking",
+  interests = {
+    -- on linkable added or removed, where linkable is adapter or plain node
+    EventInterest {
+      Constraint { "event.type", "=", "session-item-added" },
+      Constraint { "event.session-item.interface", "=", "linkable" },
+      Constraint { "item.factory.name", "c", "si-audio-adapter", "si-node" },
+    },
+  },
+  execute = function (event)
+    if tu.linkables_ready () then
+      tu.set_target_in_stream ("target.object", "defined-device-node",
+          tonumber (tu.lnkbls ["defined-device-node"].properties ["node.name"]))
+    end
+  end
+}:register ()
+
+SimpleEventHook {
+  name = "linking/test-linking",
+  after = "linking/link-target",
+  interests = {
+    EventInterest {
+      Constraint { "event.type", "=", "select-target" },
+    },
+  },
+  execute = function (event)
+    local source, om, si, si_props, si_flags, target =
+    pu:unwrap_find_target_event (event)
+
+    if not target then
+      return
+    end
+
+    Log.info (si, string.format ("handling item: %s (%s) si id(%s)",
+      tostring (si_props ["node.name"]),
+      tostring (si_props ["node.id"]), si.id))
+
+    local link = pu.lookupLink (si.id, si_flags.peer_id)
+    assert (link ~= nil)
+    assert (si_props ["node.name"] == "stream-node")
+    assert (target.properties ["node.name"] == "defined-device-node")
+    assert ((link:get_active_features () & Feature.SessionItem.ACTIVE) ~= 0)
+
+    Script:finish_activation ()
+  end
+}:register ()
