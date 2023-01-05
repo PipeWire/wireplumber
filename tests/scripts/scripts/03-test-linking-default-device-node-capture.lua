669b91678d462240a86c46bf7da46fa5f5d9e5a4
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2023-01-05 17:07:37 +0530

    linking tests: add default device test cases

--- tests/scripts/scripts/03-test-linking-default-device-node-capture.lua
+++ tests/scripts/scripts/03-test-linking-default-device-node-capture.lua
@@ -1,14 +1,15 @@
--- Tests linking of streams and non default devices. These devices nodes are
--- neither defined nor default.
+-- Tests linking of streams and default devices. Two device nodes are setup,
+-- among which only one is selected as default device node.
 
 local pu = require ("policy-utils")
 local tu = require ("test-utils")
 
 Script.async_activation = true
 
-tu.createDeviceNode ("nondefault-device-node", "Audio/Sink")
+tu.createDeviceNode ("nondefault-device-node", "Audio/Source")
+tu.createDeviceNode ("default-device-node", "Audio/Source")
 
-tu.createStreamNode ("stream-node")
+tu.createStreamNode ("stream-node", "Stream/Input/Audio")
 
 SimpleEventHook {
   name = "linking/test-linking",
@@ -20,21 +21,23 @@ SimpleEventHook {
   },
   execute = function (event)
     local source, om, si, si_props, si_flags, target =
-        pu:unwrap_find_target_event (event)
+    pu:unwrap_find_target_event (event)
 
     if not target then
       return
     end
 
     Log.info (si, string.format ("handling item: %s (%s) si id(%s)",
-        tostring (si_props ["node.name"]),
-        tostring (si_props ["node.id"]), si.id))
+      tostring (si_props ["node.name"]),
+      tostring (si_props ["node.id"]), si.id))
 
     local link = pu.lookupLink (si.id, si_flags.peer_id)
     assert (link ~= nil)
     assert (si_props ["node.name"] == "stream-node")
-    assert (target.properties ["node.name"] == "nondefault-device-node")
+    assert (target.properties ["node.name"] == "default-device-node")
     assert ((link:get_active_features () & Feature.SessionItem.ACTIVE) ~= 0)
+
+    assert (target == pu.findDefaultLinkable (si))
 
     Script:finish_activation ()
   end
