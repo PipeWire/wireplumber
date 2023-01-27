03bdf7d2832fc89695735ff973137414a18c451f
Ashok Sidipotu <ashok.sidipotu@collabora.com>
2023-01-27 06:40:38 +0530

    find-selected-default-node.lua: add a new default node hook
    Hook accords higher priority to the currently selected device over and on top of
    the previously selected devices.

--- src/scripts/default-nodes/apply-default-node.lua
+++ src/scripts/default-nodes/apply-default-node.lua
@@ -8,6 +8,7 @@ SimpleEventHook {
   name = "default-nodes/apply-default-node",
   after = { "default-nodes/find-best-default-node",
             "default-nodes/find-echo-cancel-default-node",
+            "default-nodes/find-selected-default-node",
             "default-nodes/find-stored-default-node" },
   interests = {
     EventInterest {
