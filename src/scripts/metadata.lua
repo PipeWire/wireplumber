ed224f7c798d375a8ded58e7e367b13bfb9d8bf5
George Kiagiadakis <george.kiagiadakis@collabora.com>
2023-11-07 22:07:04 +0200

    scripts: merge metadata.lua and filters-metadata.lua

--- src/scripts/metadata.lua
+++ src/scripts/metadata.lua
@@ -8,11 +8,20 @@
 
 Script.async_activation = true
 
-default_metadata = ImplMetadata ("default")
-default_metadata:activate (Features.ALL, function (m, e)
+-- note that args is a WpSpaJson
+local args = ...
+args = args:parse(1)
+
+local metadata_name = args["metadata.name"]
+
+log = Log.open_topic ("s-metadata")
+log:info ("creating metadata object: " .. metadata_name)
+
+impl_metadata = ImplMetadata (metadata_name)
+impl_metadata:activate (Features.ALL, function (m, e)
   if e then
     Script:finish_activation_with_error (
-        "failed to activate the default metadata: " .. tostring (e))
+        "failed to activate the ".. metadata_name .." metadata: " .. tostring (e))
   else
     Script:finish_activation ()
   end
