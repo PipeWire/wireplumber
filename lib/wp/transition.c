PipeWire/wireplumber

commit 18377fbf829ff25d7a8aae65542c1f57409fa7c1

transition: don't allow _return_error() to be called recursively

lib/wp/transition.c
@@ -488,10 +488,12 @@ wp_transition_return_error (WpTransition * self, GError * error)
 
   WpTransitionPrivate *priv = wp_transition_get_instance_private (self);
 
+  /* don't allow _return_error() to be called multiple times,
+     as it is dangerous to recurse in execute_step() */
   if (G_UNLIKELY (priv->error)) {
     wp_warning_object (self, "transition bailing out multiple times; "
-        "old error was: %s", priv->error->message);
-    g_clear_error (&priv->error);
+        "new error is: %s", error->message);
+    return;
   }
 
   priv->step = WP_TRANSITION_STEP_ERROR;

commit 1ddfbc532c87fb0ad18e128d574e5c3b72089416

transition: fix memory leak when error is already set
Fixes: 18377fbf829ff2 ("transition: don't allow _return_error() to be called recursively")

lib/wp/transition.c
@@ -493,6 +493,7 @@ wp_transition_return_error (WpTransition * self, GError * error)
   if (G_UNLIKELY (priv->error)) {
     wp_warning_object (self, "transition bailing out multiple times; "
         "new error is: %s", error->message);
+    g_error_free (error);
     return;
   }
 