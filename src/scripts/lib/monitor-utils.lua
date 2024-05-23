2811d46a38a3ba30e1adb4a9c5418b740d8b7369
George Kiagiadakis <george.kiagiadakis@collabora.com>
2024-05-23 12:35:23 +0300

    monitor-utils: make cam_api_data a local variable
    Otherwise it is global and it retains a reference to the `parent`
    WpSpaDevice object until this function is called again, which prevents
    some camera nodes - in some cases - from being destroyed
    
    Fixes #640

--- src/scripts/lib/monitor-utils.lua
+++ src/scripts/lib/monitor-utils.lua
@@ -161,7 +161,7 @@ function mutils.register_cam_node (self, parent, id, factory, properties)
     cam_data.dev_path = properties["api.v4l2.path"]
   end
 
-  cam_api_data = cam_data[api]
+  local cam_api_data = cam_data[api]
   cam_api_data.enum_status = true
 
   -- cache info, it comes handy when creating node
