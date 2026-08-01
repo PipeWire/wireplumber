2bef1e8e96e288b725907c7a72044c2a098d086e
Julian Bouzas <julian.bouzas@collabora.com>
2026-07-31 22:23:22 -0400

    filter-graph: Fix for graphs defined in different files for the same node
    If a user defines multiple filter graphs for the same node in different files,
    only the first one was being set. This is because the match rules callback was
    not returning true, and therefore was only handling the first iteration.
    
    Fix the iteration by returning true, and also make sure the graph index is
    always incremental so that graphs from other files are not overwritten.

--- src/scripts/node/filter-graph.lua
+++ src/scripts/node/filter-graph.lua
@@ -29,25 +29,36 @@ SimpleEventHook {
   },
   execute = function(event)
     local node = event:get_subject()
+    local base_graph_index = 0
 
     JsonUtils.match_rules (config.rules, node.properties, function (action, value)
 
       if action == "create-filter-graph" then
         local graphs = value:parse (1)
+        local n_graphs = 0
 
+        -- Build the graph params
         local graph_params = {}
         for idx, val in ipairs (graphs) do
           local index = tonumber(idx) - 1
-          local key = "audioconvert.filter-graph." .. tostring (index)
+          local key = "audioconvert.filter-graph." .. tostring (base_graph_index + index)
 
           log:info (node, "setting node filter graph param '" .. key .. "' to: " .. val)
 
           table.insert(graph_params, key)
           table.insert(graph_params, val)
+
+          n_graphs = idx
         end
 
+        -- Update base graph index
+        base_graph_index = base_graph_index + n_graphs
+
+        -- Set graphs
         setNodeFilterGraphParams (node, graph_params)
       end
+
+      return true
     end)
   end
 }:register()
